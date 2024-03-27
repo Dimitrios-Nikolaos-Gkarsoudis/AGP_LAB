7a4b32da0fb463bc741eec715e53ab42a12c39c6
Dimitrios-Nikolaos-Gkarsoudis <162180875+Dimitrios-Nikolaos-Gkarsoudis@users.noreply.github.com>
2024-03-27 21:08:05 +0200

    Update erotima2.py

--- Task1/erotima2.py
+++ Task1/erotima2.py
@@ -1,15 +1,16 @@
 import unittest
 from utils import distance, area, mean, median, stdev
-from itertools import combinations 
+from itertools import combinations
 
 points = []
-    
-with open ("points.txt", "r") as f:
+
+with open("points.txt", "r") as f:
     for line in f:
-        x,y = line.split()
+        x, y = line.split()
         points.append((int(x), int(y)))
-    
-areas = []    
+
+areas = []
+
 
 class TestPoints(unittest.TestCase):
     def setUp(self):
@@ -22,13 +23,16 @@ class TestPoints(unittest.TestCase):
             side1 = distance(triangle_points[0], triangle_points[1])
             side2 = distance(triangle_points[1], triangle_points[2])
             side3 = distance(triangle_points[2], triangle_points[0])
-            triangle_area = area(side1, side2, side3)
-            areas.append(triangle_area)
-            count = count + 1
+            if area(side1, side2, side3) != 0:
+                triangle_area = area(side1, side2, side3)
+                areas.append(triangle_area)
+                count = count + 1
         self.assertEqual(count, 161671)
-        self.assertAlmostEqual(mean(areas), 3206.86, places = 2)
-        self.assertAlmostEqual(median(areas), 2392.50, places = 2)
-        self.assertAlmostEqual(stdev(areas), 2843.23, places = 2)
+        self.assertAlmostEqual(mean(areas), 3206.86, places=2)
+        self.assertAlmostEqual(median(areas), 2392.50, places=2)
+        self.assertAlmostEqual(stdev(areas), 2843.23, places=2)
 
-if __name__ == '__main__':
-    unittest.main()       
+
+if __name__ == "__main__":
+    unittest.main()
+
